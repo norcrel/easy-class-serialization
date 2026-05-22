d71cdc5a3ed50b33e5802df2b6d8882fdc55eca4
norcrel <norcrel@gmail.com>
2026-05-22 12:08:57 -0700

    Deserializer function and serializedType decorator, updated readme for instructions.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # ts-serialization
-A small library for enabling serialization of objects with prototypes in Typescript.
+A small library for enabling serialization of objects with prototypes in Typescript using JSON.stringify().
+Intended for simple, hierarchical data model classes with some functions.
+
+# Limitations
+
+- Types are _not_ tracked in the data. They are derived via runtime prototype definitions.
+- No cyclical references.
+- No references to specific instances (deserialization only creates new instances).
+
+# Installation
+1. `npm i https://github.com/norcrel/ts-serialization.git`
+
+# Usage
+```
+import {serializedType, deserialize} from 'ts-serializer'
+
+class DataModel {
+    public description:string;
+    
+    @serializedType(OtherDataModel)
+    public other:OtherDataModel;
+}
+
+class OtherDataModel
+{
+    public x:number;
+    
+    getXString()
+    {
+        return x.toString();
+    }
+}
+
+const subModel = new OtherDataModel();
+subModel.x = 42;
+
+const dataModel = new DataModel();
+dataModel.description = "Hello!";
+dataModel.other = subModel;
+
+const serializedString = JSON.stringify(dataModel);
+
+const deserializedObj = deserialize(serializedString, DataModel.prototype);
+
+console.log(deserializedObj.other.getXString()); // "42"
+```
+
+Have a wonderful day :)
---
+++ src/ts-serializer.ts
@@ -0,0 +1,54 @@
+interface IPrototype
+{
+    prototype:object;
+}
+
+// WARNING: This must match the property used below in ReflectedClass
+const REFLECTION_PROP = "__ts-serialization-refl";
+interface ReflectedClass
+{
+    "__ts-serialization-refl"?:Map<string, WeakRef<object>>;
+}
+
+/**
+ * Decorator - records the type that should be deserialized for a field
+ */
+export function serializedType(cls:object)
+{
+    return function (target:object, fieldName:string)
+    {
+        const t = target as ReflectedClass;
+        t[REFLECTION_PROP] ||= new Map();
+        t[REFLECTION_PROP].set(fieldName, new WeakRef((cls as IPrototype).prototype));
+    };
+}
+
+/**
+ * Generalized deserialization method using a class
+ * @param {string|object} data The serialized data of the passed in object.
+ *      If string, will be deserialized with JSON.parse()
+ * @param {object} cls The prototype of the class that's being deserialized
+ * @returns {any} An instance of the class passed in (cls) with data filled
+ */
+export function deserialize(data:string|object, cls:object)
+{
+    const reflectedClass = cls as ReflectedClass;
+    const saveObj = typeof data === "string" ? JSON.parse(data) : data;
+    const instance = Object.assign(Object.create(cls), saveObj);
+    const reflectionMap = reflectedClass[REFLECTION_PROP];
+    if (reflectionMap)
+    {
+        reflectionMap.forEach((value, key) =>
+        {
+            const c = value.deref() as ReflectedClass;
+            if (!c)
+            {
+                return;
+            }
+
+            instance[key] = deserialize(JSON.stringify(instance[key]), c);
+        });
+    }
+
+    return instance;
+}
